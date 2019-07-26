38dc9ac000d61f7d2b49ed4971061992c5328f13
Garland Lai <garlandlai24@gmail.com>
2019-07-26 09:13:42 -0700

    add logicto lib files and app.rb

---
+++ spec/definition_spec.rb
@@ -0,0 +1,60 @@
+require ('pry')
+
+class Definition
+  attr_reader :id
+  attri_accessor :define, :word_id
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @define = attributes.fetch(:define)
+    @word_id = attributes.fetch(:word_id)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values
+  end
+
+  def ==(definition_to_compare)
+    (self.define() == definition_to_compare.define()) && (self.word_id() == definition_to_compare.word_id())
+  end
+
+  def self.clear
+    @@definitions = {}
+  end
+
+  def self.find(id)
+    @@defintions[id]
+  end
+
+  def create
+    @@definitions[self.id] = self
+  end
+
+  def update(define, word_id)
+    self.define = define
+    self.word_id = word_id
+    @@definitions[self.id] = self
+  end
+
+  def delete
+    @@definitions.delete(self.id)
+  end
+
+  def word
+    Word.find(self.word_id)
+  end
+
+  def self.find_by_word(wrd_id)
+    definitions = {}
+    @@definitions.values.each do |definition|
+      if definition.word_id == word_id
+        definitions.push(definition)
+      end
+    end
+    definitions
+  end
+
+
+end
