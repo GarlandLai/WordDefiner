27e16ca35787ff7b6ad4bf3fb21b7d58aa72a058
Garland Lai <garlandlai24@gmail.com>
2019-07-26 21:48:06 -0700

    add fixes to delete and update on definitions. Not sure how it was fixed.. also deleted results pages since I will not be using the search

--- app.rb
+++ app.rb
@@ -1,25 +1,25 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
-require('pry')
 require('./lib/definition')
+require('pry')
 also_reload('lib/**/*.rb')
 
-get ("/") do
+get('/') do
   @words = Word.all
   erb(:words)
 end
 
-get ('/words') do
+get('/words') do
   @words = Word.all
   erb(:words)
 end
 
-get ('/words/new') do
+get('/words/new') do
   erb(:new_word)
 end
 
-post ('/words') do
+post('/words') do
   name = params[:word_name]
   word = Word.new(name, nil)
   word.save()
@@ -27,52 +27,52 @@ post ('/words') do
   erb(:words)
 end
 
-get ('/words/:id') do
+get('/words/:id') do
   @word = Word.find(params[:id].to_i())
   erb(:word)
 end
 
-get ('/words/:id/edit') do
+get('/words/:id/edit') do
   @word = Word.find(params[:id].to_i())
   erb(:edit_word)
 end
 
-patch ('/words/:id') do
+patch('/words/:id') do
   @word = Word.find(params[:id].to_i())
   @word.update(params[:name])
   @words = Word.all
   erb(:words)
 end
 
-delete ('/words/:id') do
+delete('/words/:id') do
   @word = Word.find(params[:id].to_i())
   @word.delete()
   @words = Word.all
   erb(:words)
 end
 
-post ('/words/:id/definitions') do
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post('/words/:id/definitions') do
   @word = Word.find(params[:id].to_i())
   definition = Definition.new(params[:definition_name], @word.id, nil)
   definition.save()
   erb(:word)
 end
 
-get ('/words/:id/definitions/:definition_id') do
-  @definition = Definition.find(params[:id].to_i())
-  erb(:definition)
-end
-
-patch ('/words/:id/definitions/:definition_id') do
+patch('/words/:id/definitions/:definition_id') do
   @word = Word.find(params[:id].to_i())
   definition = Definition.find(params[:definition_id].to_i())
   definition.update(params[:name], @word.id)
   erb(:word)
 end
 
-delete ('/words/:id/definitions/:definition_id') do
-  @word = Word.find(params[:id].to_i())
+delete('/words/:id/definitions/:definition_id') do
   definition = Definition.find(params[:definition_id].to_i())
   definition.delete
+  @word = Word.find(params[:id].to_i())
   erb(:word)
 end
